cd3db42bcea3d8daeff532e1d0320163ef51d854
84Chirag <chiragvgour@gmail.com>
2023-09-05 16:54:30 +0530

    Additional Changes

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,14 @@
 //         message.classList.toggle = ('success-message')
 //     }, 3000);
 // }
+
+
+
+
+ function initMap() {
+    const mapOptions = {
+        center: { lat: 37.7749, lng: -122.4194 },
+        zoom: 12,
+    }
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
